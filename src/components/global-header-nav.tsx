89c923e960db9dc2b10349574ed62fdddcac5995
t-yng <t.yng.greenmonkey@gmail.com>
2020-07-12 12:52:56 +0900

    HeaderNavの要素をリンクにした

--- src/components/global-header-nav.tsx
+++ src/components/global-header-nav.tsx
@@ -1,18 +1,30 @@
 import React, { FC } from "react"
+import { Link } from "gatsby"
+import { css } from "@emotion/core"
 
 interface NavItem {
   id: number
   text: string
+  to: string
 }
 
 interface GlobalHeaderNavProps {
   items: NavItem[]
 }
 
+const style = {
+  link: css`
+    text-decoration: none;
+    color: inherit;
+  `,
+}
+
 const GlobalHeaderNavComponent: FC<GlobalHeaderNavProps> = ({ items }) => (
   <div>
     {items.map(item => (
-      <div key={item.id}>{item.text}</div>
+      <Link key={item.id} css={style.link} to={item.to}>
+        {item.text}
+      </Link>
     ))}
   </div>
 )
@@ -22,6 +34,7 @@ export const GlobalHeaderNav = () => {
     {
       id: 1,
       text: "Blog",
+      to: "/",
     },
   ]
 
