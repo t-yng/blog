6701234110f4d0d1bc67d64c3a6500d6fc5e1289
t-yng <t.yng.greenmonkey@gmail.com>
2020-07-18 13:49:29 +0900

    サイドバーのプロフィールを実装

--- src/components/main-side-bar.tsx
+++ src/components/main-side-bar.tsx
@@ -2,16 +2,26 @@ import React, { FC } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { css } from '@emotion/core';
 import { SideBarTags, SideBarTagsProps } from './side-bar-tags';
+import { SideBarProfile, SideBarProfileProps } from './side-bar-profile';
 
-type SideBarProps = SideBarTagsProps;
+type SideBarProps = {
+    tags: SideBarTagsProps['tags'];
+    profile: SideBarProfileProps;
+};
 
-const sidebarCss = css`
-    padding: 2rem 1rem;
-    width: 20%;
-`;
+const style = {
+    sidebar: css`
+        padding: 2rem 1rem;
+        width: 20%;
+    `,
+    section: css`
+        margin-bottom: 2rem;
+    `,
+};
 
-export const SideBarComponent: FC<SideBarProps> = ({ tags }) => (
-    <div css={sidebarCss}>
+export const SideBarComponent: FC<SideBarProps> = ({ tags, profile }) => (
+    <div css={style.sidebar}>
+        <SideBarProfile css={style.section} {...profile} />
         <SideBarTags tags={tags} />
     </div>
 );
@@ -27,12 +37,18 @@ export const SideBar = () => {
             }
         }
     `);
-    const tags: SideBarTagsProps['tags'] = data.allMarkdownRemark.group.map(
+    const tags: SideBarProps['tags'] = data.allMarkdownRemark.group.map(
         tag => ({
             name: tag.fieldValue,
             count: tag.totalCount,
         })
     );
+    const profile: SideBarProps['profile'] = {
+        name: 't-yng',
+        speciality: 'フロントエンドエンジニア',
+        avatar: 'https://via.placeholder.com/150',
+        github: 'https://github.com/t-yng',
+    };
 
-    return <SideBarComponent tags={tags} />;
+    return <SideBarComponent tags={tags} profile={profile} />;
 };
