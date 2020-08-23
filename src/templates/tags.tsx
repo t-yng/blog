cb80bb3cd7d139f53884dbdb7ebbed9b90dcb6aa
t-yng <t.yng.greenmonkey@gmail.com>
2020-08-23 14:37:22 +0900

    PostEntryコンポーネントを実装

--- src/templates/tags.tsx
+++ src/templates/tags.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from 'react';
-import { css } from '@emotion/core';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 import { Layout } from '../components/Layout';
-import { Tags } from '../components/Tags';
+import { PostEntry } from '../components/PostEntry';
 
 interface TagsProps {
     data: {
@@ -25,45 +24,17 @@ interface TagsProps {
     };
 }
 
-const style = {
-    post: css`
-        margin-bottom: 2rem;
-    `,
-    titleLink: css`
-        text-decoration: none;
-        color: inherit;
-    `,
-    date: css`
-        color: #555;
-    `,
-    border: css`
-        margin: 1.125rem 0;
-    `,
-    excerpt: css`
-        margin-bottom: 1.125rem;
-    `,
-    footer: css`
-        display: flex;
-        justify-content: space-between;
-    `,
-};
-
 const Index: FC<TagsProps> = ({ data }) => {
     return (
         <Layout>
             {data.allMarkdownRemark.edges.map(({ node }) => (
-                <div key={node.id} css={style.post}>
-                    <Link css={style.titleLink} to={node.fields.slug}>
-                        <h2>{node.frontmatter.title}</h2>
-                    </Link>
-                    <div css={style.date}>{node.frontmatter.date}</div>
-                    <hr css={style.border}></hr>
-                    <div css={style.excerpt}>{node.excerpt}</div>
-                    <div css={style.footer}>
-                        <Tags tags={node.frontmatter.tags} />
-                        <Link to={node.fields.slug}>続きを読む</Link>
-                    </div>
-                </div>
+                <PostEntry
+                    key={node.id}
+                    id={node.id}
+                    excerpt={node.excerpt}
+                    slug={node.fields.slug}
+                    frontmatter={node.frontmatter}
+                />
             ))}
         </Layout>
     );
