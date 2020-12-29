f9922435754e461d80fbd97e809507560f60d22b
t-yng <t.yng.greenmonkey@gmail.com>
2020-12-30 00:10:37 +0900

    update UsecaseContainer

--- src/usecases/UsecaseContainer.ts
+++ src/usecases/UsecaseContainer.ts
@@ -4,10 +4,13 @@ import { GetAllPosts, GetAllPostsImpl } from './GetAllPosts';
 import { GetGoupedTags, GetGroupedTagsImpl } from './GetGroupedTags';
 import { GetPostBySlug } from './GetPostBySlug/GetPostBySlug';
 import { GetPostBySlugImpl } from './GetPostBySlug/GetPostBySlugImpl';
+import { GetPostsByTag } from './GetPostsByTag/GetPostsByTag';
+import { GetPostsByTagImpl } from './GetPostsByTag/GetPostsByTagImpl';
 
 export type Usecases = {
     getAllPosts: GetAllPosts;
     getPostBySlug: GetPostBySlug;
+    getPostsByTag: GetPostsByTag;
     getGroupedTags: GetGoupedTags;
 };
 
@@ -15,6 +18,7 @@ const postsRepository: PostsRepository = new PostsRepositoryImpl();
 
 const getAllPostsImpl: GetAllPosts = new GetAllPostsImpl(postsRepository);
 const getPostBySlugImpl: GetPostBySlug = new GetPostBySlugImpl(postsRepository);
+const getPostsByTagImpl: GetPostsByTag = new GetPostsByTagImpl(postsRepository);
 const getGroupedTagsImpl: GetGoupedTags = new GetGroupedTagsImpl(
     postsRepository
 );
@@ -22,5 +26,6 @@ const getGroupedTagsImpl: GetGoupedTags = new GetGroupedTagsImpl(
 export const usecases: Usecases = {
     getAllPosts: getAllPostsImpl,
     getPostBySlug: getPostBySlugImpl,
+    getPostsByTag: getPostsByTagImpl,
     getGroupedTags: getGroupedTagsImpl,
 };
