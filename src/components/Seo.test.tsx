04ce8fb113a1856cb51d7a9cf3a711feb5cf081f
t-yng <t.yng.greenmonkey@gmail.com>
2020-12-29 16:33:50 +0900

    fix Seo

---
+++ src/components/Seo.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render, wait, waitFor } from '@testing-library/react';
+import { Seo, SeoProps } from './Seo';
+
+describe('Seo', () => {
+    const title = 'page title';
+    const description = 'page description';
+    const author = 'page author';
+
+    const renderSeo = (props: SeoProps) => {
+        return render(<Seo {...{ ...props }} />);
+    };
+
+    it('render title', async () => {
+        renderSeo({ title, description, author });
+
+        await waitFor(() => {
+            // react-helmetの仕様で非同期で描画されるので、documentオブジェクトで参照する
+            const titleElement = document.querySelector('title');
+            expect(titleElement).toBeInTheDocument();
+            expect(titleElement).toHaveTextContent(title);
+        });
+    });
+
+    it('render description', async () => {
+        renderSeo({ title, description, author });
+
+        await waitFor(() => {
+            const meta = document.querySelector('meta[name="description"]');
+            expect(meta).toBeInTheDocument();
+            expect(meta).toHaveAttribute('content', description);
+        });
+    });
+
+    it('render author', async () => {
+        renderSeo({ title, description, author });
+
+        await waitFor(() => {
+            const meta = document.querySelector('meta[name="author"]');
+            expect(meta).toBeInTheDocument();
+            expect(meta).toHaveAttribute('content', author);
+        });
+    });
+});
--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,50 +1,16 @@
 import React, { FC } from 'react';
 import { Helmet } from 'react-helmet';
-import { useLocation } from '@reach/router';
-import { useStaticQuery, graphql } from 'gatsby';
 
-interface SeoProps {
-    title?: string;
-    author?: string;
-    description?: string;
-}
-
-export const SEO: FC<SeoProps> = ({ title, description }) => {
-    const { pathname } = useLocation();
-    const { site } = useStaticQuery(query);
-
-    const {
-        defaultTitle,
-        defaultDescription,
-        siteUrl,
-        author,
-    } = site.siteMetadata;
-
-    const seo = {
-        title: title ?? defaultTitle,
-        author: author,
-        description: description ?? defaultDescription,
-        url: `${siteUrl}${pathname}`,
-    };
-
-    return (
-        <Helmet title={seo.title}>
-            <html lang="ja" />
-            <meta name="author" content={seo.author} />
-            <meta name="description" content={seo.description} />
-        </Helmet>
-    );
+export type SeoProps = {
+    title: string;
+    description: string;
+    author: string;
 };
 
-const query = graphql`
-    query SEO {
-        site {
-            siteMetadata {
-                author: author
-                defaultTitle: title
-                defaultDescription: description
-                siteUrl: url
-            }
-        }
-    }
-`;
+export const Seo: FC<SeoProps> = ({ title, description, author }) => (
+    <Helmet title={title}>
+        <html lang="ja" />
+        <meta name="author" content={author} />
+        <meta name="description" content={description} />
+    </Helmet>
+);
