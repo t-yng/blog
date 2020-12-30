dc2ad66639abe158536550380733b9b70411b4ac
t-yng <t.yng.greenmonkey@gmail.com>
2020-12-30 21:55:24 +0900

    add Post component

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,69 +1,19 @@
 import React, { FC } from 'react';
-import { css } from '@emotion/react';
 import {
     GetStaticPathsResult,
     GetStaticProps,
     GetStaticPropsResult,
 } from 'next';
-import ReactMarkdown, { ReactMarkdownProps } from 'react-markdown';
 import { Layout } from '../../components/Layout';
-import { Tags } from '../../components/Tags';
 import { Seo } from '../../components/Seo';
-import { colors } from '../../styles/color';
+import { Post } from '../../components/Post';
 import { Tag } from '../../entities/Tag';
+import { Post as PostEntity } from '../../entities/Post';
 import { usecases } from '../../usecases/UsecaseContainer';
-import { Post } from '../../entities/Post';
-
-const style = {
-    post: css`
-        background-color: ${colors.white};
-        border: 1px solid ${colors.black6};
-        padding: 1rem;
-    `,
-    header: css`
-        margin-bottom: 2rem;
-    `,
-    title: css`
-        margin-bottom: 0.75rem;
-    `,
-    content: css`
-        h2,
-        h3,
-        h4 {
-            margin-top: 2.5rem;
-        }
-        h2 {
-            border-bottom: 0.5px solid ${colors.black4};
-            padding-bottom: 0.5rem;
-        }
-        blockquote {
-            border-left: 5px solid ${colors.black5};
-            color: ${colors.black2};
-            padding: 1rem;
-            padding-right: 0;
-            margin: 1.5rem 0;
-        }
-    `,
-    image: css`
-        max-width: 80%;
-    `,
-    picture: css`
-        display: inline-block;
-        text-align: center;
-    `,
-};
 
 type PostPageProps = {
-    post: Post;
+    post: PostEntity;
     tags: Tag[];
-};
-
-const rederers: ReactMarkdownProps['renderers'] = {
-    image: ({ src, alt }) => (
-        <picture css={style.picture}>
-            <img alt={alt} src={src} css={style.image} />
-        </picture>
-    ),
 };
 
 const PostPage: FC<PostPageProps> = ({ post, tags }) => (
@@ -73,17 +23,7 @@ const PostPage: FC<PostPageProps> = ({ post, tags }) => (
             description={post.description}
             author={post.author}
         />
-        <div css={style.post}>
-            <header css={style.header}>
-                <h1 css={style.title}>{post.title}</h1>
-                <Tags tags={post.tags} />
-            </header>
-            <ReactMarkdown
-                css={style.content}
-                renderers={rederers}
-                children={post.content}
-            />
-        </div>
+        <Post post={post} />
     </Layout>
 );
 
