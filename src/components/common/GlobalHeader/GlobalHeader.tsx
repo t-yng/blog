7ba0f0a0f821be076c73e3ba5d082b5b6d7e74a7
t-yng <t.yng.greenmonkey@gmail.com>
2021-01-08 23:23:43 +0900

    remove widh:100vw of header

--- src/components/common/GlobalHeader/GlobalHeader.tsx
+++ src/components/common/GlobalHeader/GlobalHeader.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { css } from '@emotion/react';
 import { GlobalHeaderMain } from './GlobalHeaderMain';
 
-const style = {
-    globalHeader: css`
-        width: 100vw;
-    `,
-};
-
 export const GlobalHeader = () => (
-    <header css={style.globalHeader}>
+    <header>
         <GlobalHeaderMain />
     </header>
 );
