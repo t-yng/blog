d672879266c5ea574b2bc8a37a948596a7e57a56
t-yng <t.yng.greenmonkey@gmail.com>
2021-06-13 11:45:51 +0900

    Next.jsのWebpackのバージョンをv5に変更

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
 const config = {
+    future: {
+        webpack5: true,
+    },
     webpack: (config, { isServer }) => {
         // fix Can't resolve 'fs'
         // @see: https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
         if (!isServer) {
-            config.node = {
-                fs: 'empty',
-            };
+            config.resolve.fallback.fs = false;
         }
 
         return config;
