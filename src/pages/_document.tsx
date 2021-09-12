e8fdd2eaf888733c295c6489c3b897fe49c7f3e3
t-yng <t.yng.greenmonkey@gmail.com>
2021-09-12 10:40:17 +0900

    tagmanagerの読み込みをasyncに戻した

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
                 <Head>
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
-                        defer
+                        async
                         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
                     />
                     <script
