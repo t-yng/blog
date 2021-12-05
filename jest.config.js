da4eb038cd1751e9bfb0aea855d9f73fc2109e55
t-yng <t.yng.greenmonkey@gmail.com>
2021-12-05 13:09:50 +0900

    jestのコード変換を@swc/jestに変更

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,26 @@
 module.exports = {
-    preset: 'ts-jest/presets/js-with-babel',
+    transform: {
+        '^.+\\.(t|j)sx?$': [
+            '@swc/jest',
+            {
+                sourceMaps: true,
+                module: {
+                    type: 'commonjs',
+                },
+                jsc: {
+                    parser: {
+                        syntax: 'typescript',
+                        tsx: true,
+                    },
+                    transform: {
+                        react: {
+                            runtime: 'automatic',
+                        },
+                    },
+                },
+            },
+        ],
+    },
     testEnvironment: 'jsdom',
     transformIgnorePatterns: ['/node_modules/(?!react-markdown)/'],
     testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)'],
@@ -11,9 +32,4 @@ module.exports = {
         '<rootDir>/src/constants/',
         'index.ts',
     ],
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.test.json',
-        },
-    },
 };
