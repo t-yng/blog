db3d0b41013354edcadd50ea70ea5b377ea6d2e8
t-yng <t.yng.greenmonkey@gmail.com>
2022-02-11 11:32:07 +0900

    chore: GA4のトラッキングコードを追加

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,4 +1,5 @@
 export const GA_TRACKING_ID = 'UA-57390966-4';
+export const GA4_TRACKING_ID = 'G-02GKP2XYRY';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
