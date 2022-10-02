014d7a60de80c5b8f064d6a23db159c0b11d3713
t-yng <t.yng.greenmonkey@gmail.com>
2022-10-02 22:09:58 +0900

    markdownの変換をmarkdown-itに変更

--- src/components/post/Post/Post.tsx
+++ src/components/post/Post/Post.tsx
@@ -1,86 +1,12 @@
 import { FC } from 'react';
-import ReactMarkdown, { Options } from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import remarkBreaks from 'remark-breaks';
-import rehypeRaw from 'rehype-raw';
-import Image from 'next/image';
 import { Post as PostEntity } from '@/entities/Post';
 import { Tags } from '@/components/common';
 import * as style from './Post.css';
 import { formatDate } from '@/lib/format';
-import { Code } from '../Code';
+import 'prismjs/themes/prism-tomorrow.css';
 
 type PostProps = {
     post: PostEntity;
-};
-
-const components: Options['components'] = {
-    img: ({ src, width, height, alt }) => {
-        if (src == null || src === '') return null;
-
-        return (
-            <div className={style.imageWrapper}>
-                <Image
-                    src={src || ''}
-                    alt={alt}
-                    width={width}
-                    height={height}
-                />
-            </div>
-        );
-    },
-
-    // NOTE: props に node が含まれるため、node="[object Object]" がタグの属性として出力されるのを防ぐ
-    //       ために node を受け取っている
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    code: ({ node, inline, className, children, ...props }) => {
-        const match = /language-(\w+)/.exec(className || '');
-
-        return !inline && match ? (
-            <Code
-                code={String(children).replace(/\n$/, '')}
-                language={match[1]}
-            />
-        ) : (
-            <code className={className} {...props}>
-                {children}
-            </code>
-        );
-    },
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    a: ({ node, children, ...props }) => {
-        const isExternalLink =
-            props.href != null ? /^http/.test(props.href) : false;
-
-        return (
-            <a
-                target={isExternalLink ? '_blank' : undefined}
-                rel="noopner noreferrer"
-                {...props}
-            >
-                {children}
-            </a>
-        );
-    },
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    h2: ({ node, children, ...props }) => {
-        return (
-            <h2 id={String(children)} {...props}>
-                {children}
-            </h2>
-        );
-    },
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    h3: ({ node, children, ...props }) => {
-        return (
-            <h3 id={String(children)} {...props}>
-                {children}
-            </h3>
-        );
-    },
 };
 
 export const Post: FC<PostProps> = ({ post }) => {
@@ -92,14 +18,10 @@ export const Post: FC<PostProps> = ({ post }) => {
                 <Tags tags={post.tags} />
             </header>
 
-            <ReactMarkdown
+            <div
+                dangerouslySetInnerHTML={{ __html: post.content }}
                 className={style.content}
-                components={components}
-                rehypePlugins={[rehypeRaw]}
-                remarkPlugins={[remarkGfm, remarkBreaks]}
-                data-testid="content"
-                // eslint-disable-next-line react/no-children-prop
-                children={post.content}
+                data-test="content"
             />
         </div>
     );
--- src/repositories/PostsRepositoryImpl.ts
+++ src/repositories/PostsRepositoryImpl.ts
@@ -6,6 +6,8 @@ import { Post } from '@/entities';
 import { PostsRepository } from './PostsRepository';
 import { profile } from '@/config/profile';
 import { parseImageTextWithSize } from '@/lib/markdown';
+import MarkdownIt from 'markdown-it';
+import Prism from 'prismjs';
 
 export class PostsRepositoryImpl implements PostsRepository {
     getAllPosts(): Post[] {
@@ -30,7 +32,62 @@ export class PostsRepositoryImpl implements PostsRepository {
         const markdown = matter(fileContent);
         const { data } = markdown;
         let { content } = markdown;
+        const md = new MarkdownIt({
+            html: true,
+            breaks: true,
+            highlight: (str, lang) => {
+                if (lang && Prism.languages[lang]) {
+                    const html = Prism.highlight(
+                        str,
+                        Prism.languages[lang],
+                        lang
+                    );
+
+                    return `<pre class="language-${lang}"><code class="language-${lang}">${html}</code></pre>`;
+                }
+                return '';
+            },
+        });
+
+        // TODO: imgタグにlazy loading の設定を追加
+        md.renderer.rules.image = (tokens, idx, options, _env, self) => {
+            const token = tokens[idx];
+            token.attrSet('loading', 'lazy');
+
+            return self.renderToken(tokens, idx, options);
+        };
+
+        // 外部リンクの場合は別タブで表示
+        md.renderer.rules['link_open'] = (tokens, idx, options, env, self) => {
+            const token = tokens[idx];
+            const result = token.attrs?.find((attr) => attr[0] === 'href');
+            const href = result?.[1];
+            const isExternalLink = href != null ? /^http/.test(href) : false;
+
+            if (isExternalLink) {
+                token.attrSet('target', '_blank');
+            }
+
+            return self.renderToken(tokens, idx, options);
+        };
+
+        // heading tag にアンカーリンクを追加
+        md.renderer.rules['heading_open'] = (
+            tokens,
+            idx,
+            options,
+            _env,
+            self
+        ) => {
+            const token = tokens[idx];
+            const nextToken = tokens[idx + 1];
+
+            token.attrSet('id', nextToken.content);
+
+            return self.renderToken(tokens, idx, options);
+        };
         content = parseImageTextWithSize(content);
+        content = md.render(content);
 
         return {
             id: crypto.createHash('md5').update(slug).digest('hex'),
