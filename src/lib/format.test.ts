eb96ed0cbcc303377241b7b7b2b1f705d998ba31
t-yng <t.yng.greenmonkey@gmail.com>
2022-12-17 04:23:14 +0900

    テスト修正

--- src/lib/format.test.ts
+++ src/lib/format.test.ts
@@ -5,7 +5,7 @@ describe('format', () => {
         it('format year年 month月 day日', () => {
             const date = '2020-04-19T00:00:00.000Z';
             const result = formatDate(date);
-            expect(result).toBe('2020年 04月 19日');
+            expect(result).toBe('2020年04月19日');
         });
     });
 });
