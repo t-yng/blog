9314b16ba34b013cf140c2478e4569c731658436
t-yng <t.yng.greenmonkey@gmail.com>
2022-12-16 22:27:31 +0900

    タグにaria-labelを追加

--- src/components/common/Tag/Tag.tsx
+++ src/components/common/Tag/Tag.tsx
@@ -8,7 +8,11 @@ interface TagProps {
 }
 
 export const Tag: FC<TagProps> = ({ name }) => (
-    <Link href={createTagLink(name)} decoration={false}>
+    <Link
+        href={createTagLink(name)}
+        decoration={false}
+        anchorProps={{ 'aria-label': `タグ、${name}` }}
+    >
         <div className={style.tag}>{name}</div>
     </Link>
 );
