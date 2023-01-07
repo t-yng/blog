c713adfd437fbb2f177d5c27ae1755c0b52663d6
t-yng <t-yng@users.noreply.github.com>
2023-01-07 19:07:27 +0900

    テスト実行をjestからVitestに変更 (#445)

---
+++ vitest.config.ts
@@ -0,0 +1,37 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+import '@vanilla-extract/css/disableRuntimeStyles';
+
+export default defineConfig({
+    test: {
+        globals: true,
+        // testEnvironment
+        environment: 'jsdom',
+        // setupFilesAfterEnv
+        setupFiles: [`test/setup.ts`],
+        // testMatch
+        include: [`**/*.{test,spec}.{ts,tsx}`],
+        // moduleNameMapper
+        alias: {
+            '@/': `${__dirname}/src/`,
+            '@test/': `${__dirname}/test/`,
+            '\\.css$': 'identity-obj-proxy',
+        },
+        coverage: {
+            // collectCoverageFrom
+            include: [`src/**/*.{ts,tsx}`],
+            // coveragePathIgnorePatterns
+            exclude: [
+                `src/usecases/UsecaseContainer.ts`,
+                `src/styles/`,
+                `src/constants/`,
+                `src/**/index.ts`,
+            ],
+        },
+    },
+    // transform
+    plugins: [vanillaExtractPlugin(), react()],
+});
