50e3e7c97bb4d88860e3203a17a692c42095fc35
t-yng <t.yng.greenmonkey@gmail.com>
2023-05-25 20:24:53 +0900

    vitestのテスト対象のファイルパスを修正

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         // setupFilesAfterEnv
         setupFiles: [`test/setup.ts`],
         // testMatch
-        include: [`**/*.{test,spec}.{ts,tsx}`],
+        include: [`src/**/*.{test,spec}.{ts,tsx}`],
         // moduleNameMapper
         alias: {
             '@/': `${__dirname}/src/`,
