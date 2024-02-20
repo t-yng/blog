6f24c679a1c9c3173b17ca2c1d97564ea8bc22e4
t-yng <t.yng.greenmonkey@gmail.com>
2024-02-20 23:51:02 +0900

    E2Eのスナップショットをアップデート

--- e2e/tag.spec.ts
+++ e2e/tag.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('visual regression', async ({ page }) => {
-  await page.goto('/post/フロントエンド');
+  await page.goto('/tag/フロントエンド');
   await expect(page).toHaveScreenshot({
     fullPage: true,
     animations: 'disabled',
